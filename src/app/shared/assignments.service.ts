aecfb511c0af48c3aa249d0def99fe05e4908b50
capatainkomic <magiqueram@gmail.com>
2025-06-02 06:04:21 +0200

    render

--- src/app/shared/assignments.service.ts
+++ src/app/shared/assignments.service.ts
@@ -11,7 +11,7 @@ import { bdInitialAssignments } from './data';
 export class AssignmentsService {
   //backendURL = 'http://localhost:8010/api/assignments';
   // backendURL = 'https://angularbackm2mbdsesatic2024-2025.onrender.com/api/assignments';
-  backendURL = 'https://angular-back-gxb9.onrender.com/api/assignments';
+  private backendURL = 'https://angular-back-gxb9.onrender.com/api/assignments';
 
 
 assignments:Assignment[] = [];
